31246c7bf09c73d695613684678975288a892e7e
benjamin <benjaminyakobi@gmail.com>
2026-03-25 21:47:57 +0200

    feat:ref: updated lua functions query

--- queries/lua/functions.scm
+++ queries/lua/functions.scm
@@ -1,3 +1,23 @@
+;; Named functions
 (function_declaration
-name: (identifier) @name
-parameters: (parameters) @params) @function
+  name: (_) @name
+  parameters: (parameters) @params) @func
+
+;; Anonymous
+(function_definition
+  parameters: (parameters) @params) @func
+
+;; local foo = function() end
+(variable_declaration
+  (assignment_statement
+    (variable_list (identifier) @name)
+    (expression_list
+      (function_definition
+        parameters: (parameters) @params)))) @func
+
+;; foo = function() end
+(assignment_statement
+  (variable_list (identifier) @name)
+  (expression_list
+    (function_definition
+      parameters: (parameters) @params))) @func
