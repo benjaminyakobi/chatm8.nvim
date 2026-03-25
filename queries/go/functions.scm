bb0acbf98bae50fcb26c6e5d76deaa4853139d2f
benjamin <benjaminyakobi@gmail.com>
2026-03-25 21:48:04 +0200

    feat:ref: updated go functions query

--- queries/go/functions.scm
+++ queries/go/functions.scm
@@ -1,4 +1,33 @@
+;; function
 (function_declaration
-name: (identifier) @name
-parameters: (parameter_list) @params
-result: (parameter_list)? @return) @function
+  name: (identifier) @name
+  parameters: (parameter_list) @params
+  result: (_) ? @return) @func
+
+;; method
+(method_declaration
+  receiver: (parameter_list) @receiver
+  name: (field_identifier) @name
+  parameters: (parameter_list) @params
+  result: (_) ? @return) @func
+
+;; anonymous
+(func_literal
+  parameters: (parameter_list) @params
+  result: (_) ? @return) @func
+
+;; short assign
+(short_var_declaration
+  left: (expression_list (identifier) @name)
+  right: (expression_list
+    (func_literal
+      parameters: (parameter_list) @params
+      result: (_) ? @return))) @func
+
+;; var assign
+(var_spec
+  name: (identifier) @name
+  value: (expression_list
+    (func_literal
+      parameters: (parameter_list) @params
+      result: (_) ? @return))) @func
