add437a181675f260d13d5f572a81c882a90f163
benjamin <benjaminyakobi@gmail.com>
2026-03-28 23:13:42 +0300

    feat: improved python queries for func signatures

--- queries/python/functions.scm
+++ queries/python/functions.scm
@@ -1,3 +1,19 @@
+;; Top-level & nested functions
 (function_definition
-name: (identifier) @name
-parameters: (parameters) @params) @function
+  name: (identifier) @name
+  parameters: (parameters) @params) @func
+
+;; Methods inside classes (same node type, but still useful to capture)
+(function_definition
+  name: (identifier) @name
+  parameters: (parameters) @params) @func
+
+;; Lambda (anonymous function)
+(lambda
+  parameters: (lambda_parameters)? @params) @func
+
+;; Assigned lambda: f = lambda x: x + 1
+(assignment
+  left: (identifier) @name
+  right: (lambda
+    parameters: (lambda_parameters)? @params)) @func
