ea0e51657264756aa73020c27a0ff0e262263b25
benjamin <benjaminyakobi@gmail.com>
2026-05-11 17:13:18 +0300

    feat:ref: updated 8? help output

--- lua/chat.lua
+++ lua/chat.lua
@@ -26,15 +26,6 @@ function M.setup(opts)
   end
 
   local help = [[
-<Leader>8p: Prompt Interface
-  1. [Visual Mode] Select text (either code snippets or natural language 
-     instructions) and press <Leader>8p to open the prompt window.
-  2. [Layout] Two windows will open:
-     - Upper window: conversation history (read-only).
-     - Lower window: prompt input.
-  3. [Navigation] Press <C-s> in Normal or Visual mode to switch between the
-     history window and the prompt window.
-
 <Leader>8i: Inline Implementation
   1. [Visual Mode] Select text (either code snippets or natural language instructions).
   2. Press <Leader>8i to automatically complete the implementation or transform 
@@ -44,7 +35,12 @@ function M.setup(opts)
   1. [Normal/Visual Mode] Press `<Leader>8c` to toggle a persistent chat window 
      in a split view.
   2. [Chat Window] Maintain a continuous, multi-turn conversation with the model 
-     that persists across different files and buffers.]]
+     that persists across different files and buffers.
+    2.1. [Layout] Two windows will open:
+         - Upper window: conversation history (read-only).
+         - Lower window: prompt input.
+    2.2. [Navigation] Press <C-s> in Normal or Visual mode to switch between the
+         history window and the prompt window.]]
 
   vim.keymap.set("n", "<Leader>8?", function()
     if opts.dev then
