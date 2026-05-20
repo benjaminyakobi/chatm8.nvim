cce3429eed50b16f2e1cfef7ea5a1fef92111daf
benjamin <benjaminyakobi@gmail.com>
2026-05-20 17:03:20 +0300

    feat: added two basic functions to get & clear history, add still not implemented

--- lua/chat/history.lua
+++ lua/chat/history.lua
@@ -1,1 +1,18 @@
 -- NOTE: managing canonical provider-agnostic history
+local M = {}
+
+local history = {}
+
+function M.get()
+  return vim.deepcopy(history)
+end
+
+function M.clear()
+  history = {}
+end
+
+function M.add(role, message)
+  -- TODO: impelment
+end
+
+return M
