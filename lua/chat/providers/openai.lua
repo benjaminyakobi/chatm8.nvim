d321faffb7398aa0114b26d7c7a5de3fb17c488c
benjamin <benjaminyakobi@gmail.com>
2026-05-26 18:17:38 +0300

    ref: commented out summarize_conversation func (using answer for every prompt including summarization), also updated error messages

--- lua/chat/providers/openai.lua
+++ lua/chat/providers/openai.lua
@@ -47,7 +47,7 @@ function M.answer(prompt, callback)
   local ok_encode, json = pcall(vim.json.encode, body)
   if not ok_encode then
     callback({
-      error = "chat.nvim: JSON encode failed: " .. json,
+      error = "JSON encode failed: " .. json,
     })
     return
   end
@@ -66,7 +66,7 @@ function M.answer(prompt, callback)
   }, { text = true }, function(res)
     if res.code ~= 0 then
       callback({
-        error = "chat.nvim: Request failed: " .. (res.stderr or "unknown error"),
+        error = "Request failed: " .. (res.stderr or "unknown error"),
       })
       return
     end
@@ -75,7 +75,7 @@ function M.answer(prompt, callback)
 
     if not ok_decode then
       callback({
-        error = "chat.nvim: JSON decode failed",
+        error = "JSON decode failed",
       })
       return
     end
@@ -86,7 +86,7 @@ function M.answer(prompt, callback)
 
     if not ok_extract then
       callback({
-        error = "chat.nvim: Invalid response structure",
+        error = "Invalid response structure",
       })
       return
     end
@@ -97,66 +97,66 @@ function M.answer(prompt, callback)
   end)
 end
 
----@return nil
----@param prompt_history table
----@param callback function
-function M.summarize_conversation(prompt_history, callback)
-  local body = {
-    model = providers.openai_model,
-    messages = normalize_prompt(prompt_history),
-  }
-
-  local ok_encode, json = pcall(vim.json.encode, body)
-  if not ok_encode then
-    callback({
-      error = "chat.nvim: Failed to summarize history, JSON encode failed: " .. json,
-    })
-    return
-  end
-
-  vim.system({
-    "curl",
-    "https://api.openai.com/v1/chat/completions",
-    "-H",
-    "Content-Type: application/json",
-    "-H",
-    "Authorization: Bearer " .. providers.api_key,
-    "-X",
-    "POST",
-    "-d",
-    json,
-  }, { text = true }, function(res)
-    if res.code ~= 0 then
-      callback({
-        error = "chat.nvim: Failed to summarize history, Request failed: " .. (res.stderr or "unknown error"),
-      })
-      return
-    end
-
-    local ok_decode, data = pcall(vim.json.decode, res.stdout)
-
-    if not ok_decode then
-      callback({
-        error = "chat.nvim: Failed to summarize history, JSON decode failed",
-      })
-      return
-    end
-
-    local ok_extract, text = pcall(function()
-      return data.choices[1].message.content
-    end)
-
-    if not ok_extract then
-      callback({
-        error = "chat.nvim: Failed to summarize history, Invalid response structure",
-      })
-      return
-    end
-
-    callback({
-      content = text,
-    })
-  end)
-end
+-- ---@return nil
+-- ---@param prompt_history table
+-- ---@param callback function
+-- function M.summarize_conversation(prompt_history, callback)
+--   local body = {
+--     model = providers.openai_model,
+--     messages = normalize_prompt(prompt_history),
+--   }
+--
+--   local ok_encode, json = pcall(vim.json.encode, body)
+--   if not ok_encode then
+--     callback({
+--       error = "JSON encode failed: " .. json,
+--     })
+--     return
+--   end
+--
+--   vim.system({
+--     "curl",
+--     "https://api.openai.com/v1/chat/completions",
+--     "-H",
+--     "Content-Type: application/json",
+--     "-H",
+--     "Authorization: Bearer " .. providers.api_key,
+--     "-X",
+--     "POST",
+--     "-d",
+--     json,
+--   }, { text = true }, function(res)
+--     if res.code ~= 0 then
+--       callback({
+--         error = "Request failed: " .. (res.stderr or "unknown error"),
+--       })
+--       return
+--     end
+--
+--     local ok_decode, data = pcall(vim.json.decode, res.stdout)
+--
+--     if not ok_decode then
+--       callback({
+--         error = "JSON decode failed",
+--       })
+--       return
+--     end
+--
+--     local ok_extract, text = pcall(function()
+--       return data.choices[1].message.content
+--     end)
+--
+--     if not ok_extract then
+--       callback({
+--         error = "Invalid response structure",
+--       })
+--       return
+--     end
+--
+--     callback({
+--       content = text,
+--     })
+--   end)
+-- end
 
 return M
