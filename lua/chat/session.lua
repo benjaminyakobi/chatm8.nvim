9beeecdfe0405fa49d48ac8cc07712e21a099bce
benjamin <benjaminyakobi@gmail.com>
2026-06-28 13:41:20 +0300

    feat: new session class with a constructor

--- lua/chat/session.lua
+++ lua/chat/session.lua
@@ -1,1 +1,17 @@
 -- NOTE: this file manages chat sessions
+
+local Session = {}
+
+Session.__index = Session -- class
+
+---@return metatable
+function Session.new() -- constructor
+  local ts = os.time()
+  return setmetatable({
+    id = ts,
+    created_at = ts,
+    updated_at = ts,
+    title = "New Chat",
+    messages = {},
+  }, Session)
+end
