f998468bb6743a985222164e3c31191fe217d678
benjamin <benjaminyakobi@gmail.com>
2026-07-08 16:45:39 +0300

    feat: commented func signatures that should be implemented

--- lua/chat/storage.lua
+++ lua/chat/storage.lua
@@ -1,1 +1,13 @@
 -- NOTE: this module should handle saving/loading sessions to/from disk
+
+local M = {}
+
+-- function M.save_session() end
+--
+-- function M.load_session() end
+--
+-- function M.delete_session() end
+--
+-- function M.list_sessions() end
+
+return M
