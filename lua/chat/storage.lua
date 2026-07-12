62ac27c8080d53d8985f0b68048a1cecb0f82665
benjamin <benjaminyakobi@gmail.com>
2026-07-12 17:42:51 +0300

    ref: updated public api commented code before actual implementation

--- lua/chat/storage.lua
+++ lua/chat/storage.lua
@@ -95,19 +95,12 @@ end
 -- Public API
 ----------------------------------
 
--- TODO: implelemt
--- ---@param session Session
 -- function M.save_session(session) end
---
--- TODO: implelemt
--- ---@param id integer
+
 -- function M.load_session(id) end
---
--- TODO: implelemt
--- ---@param id integer
+
 -- function M.delete_session(id) end
---
--- TODO: implelemt
+
 -- function M.list_sessions() end
 
 return M
